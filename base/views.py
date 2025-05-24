d14a29e5dfc8aca921ceeb35d624c8130470f7a4
Andres Chitan <felipechitan07@gmail.com>
2025-05-23 19:28:04 -0500

    Actualizar vistas, URLs y agregar login.html

--- base/views.py
+++ base/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from . models import Tarea
 # Create your views here.
+
+class Logueo(LoginView):
+    template_name = 'login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    def get_success_url(self):
+        return reverse_lazy('tareas') #Cuando termine tiene que ir a esa página (que lo devuelve al listado de tareas)
 
 class ListaPendientes(ListView):
     model = Tarea
